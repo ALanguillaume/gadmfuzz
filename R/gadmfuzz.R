13214017b8b415c2e2d8f38bdd77d33c8980faa5
ALanguillaume <antoinelanguillaume@gmail.com>
2021-03-30 21:50:22 +0200

    Let there be light

---
+++ R/gadmfuzz.R
@@ -0,0 +1,74 @@
+
+#' @importFrom stringi stri_trans_general
+to_Latin_ASCII <- function(string) {
+  stringi::stri_trans_general(string, id = "Latin-ASCII")
+}
+
+#' Identify level of gadm polygon
+#' @param gadm_country [sf object] gadm polygon geometries for a given country.
+#' @return [integer] 0, 1 or 2
+#' @importFrom stringr str_extract
+determine_level_gadm <- function(gadm_country) {
+  NAME_X <- grep("^NAME_\\d$", names(gadm_country), value = TRUE)
+  level_gadm <- max(as.integer(stringr::str_extract(NAME_X, "\\d")))
+  return(level_gadm)
+}
+
+#' Get all english spellings of a given gadm polygon regions
+#' @param gadm_country A sf object holding geometries for a given gadm country.
+#' @importFrom purrr map reduce
+get_all_english_spellings <- function(gadm_country) {
+  level_gadm <- determine_level_gadm(gadm_country)
+  subregion_name_cols <- paste0(c("NAME_", "VARNAME_"), level_gadm)
+  target_cols <- gadm_country[, subregion_name_cols, drop = TRUE]
+  target_cols <- purrr::map(target_cols, to_Latin_ASCII)
+  combined_vector <- purrr::reduce(function(x, y) paste(x, y, sep = "|"), target_cols)
+  combined_list <- strsplit(combined_vector, "\\|")
+  return(combined_list)
+}
+
+#' Perform fuzzy string
+#' @import stringdist stringdist
+#' @importFrom purrr map_dfr
+fuzzy_string_match <- function(national_stats, gadm) {
+  df_dist <- expand.grid(national_stats = national_stats, gadm = gadm)
+  df_dist$dist <- stringdist::stringdist(df_dist$national_stats, df_dist$gadm,
+                                         method = "jw")
+  l_df_dist <- split(df_dist, df_dist$gadm)
+  df_dist_match <- purrr::map_dfr(l_df_dist, ~ .x[which.min(.x$dist), ])
+  return(df_dist_match)
+}
+
+#' @importFrom dplyr group_by filter ungroup
+#' @importFrom purrr map
+find_best_match <- function(dist_table_df) {
+  dist_table_by_ns <- dplyr::group_by(dist_table_df, national_stats)
+  best_matches <- dplyr::filter(dist_table_by_ns, dist == min(dist))
+  best_matches <- dplyr::ungroup(best_matches)
+  best_matches[c("national_stats", "gadm")] <-
+    purrr::map(best_matches[c("national_stats", "gadm")], as.character)
+  return(best_matches)
+}
+
+#' @importFrom sf st_drop_geometry
+#' @importFrom purrr map map_dfr map2_dfr
+match_subregion_names <- function(gadm_sf, gadm_id_subregion,
+                                  gadm_entries_list, subregion_names) {
+
+  gadm <- sf::st_drop_geometry(gadm_sf)
+  gadm$dist_table <-
+    purrr::map(gadm[[gadm_entries_list]],
+               function(gadm_entries) {
+                 purrr::map_dfr(subregion_names,
+                                fuzzy_string_match,
+                                gadm_entries)
+               })
+  dist_table_df <-
+    purrr::map2_dfr(gadm$dist_table, gadm[[gadm_id_subregion]],
+                    function(dist_table, id_subregion) {
+                      dist_table[["id_subregion"]] <- id_subregion
+                      return(dist_table)
+                    })
+  best_matches <- find_best_match(dist_table_df)
+  return(best_matches)
+}
